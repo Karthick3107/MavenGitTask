a8e7df80ccef2bc439ec64dba43811c8e4751c17
Karthick3107 <karthickbharthi95@gmail.com>
2022-01-01 11:40:44 +0530

    Password Fuctionality

--- src/org/com/GitTaskOne.java
+++ src/org/com/GitTaskOne.java
@@ -1,5 +1,13 @@
 package org.com;
 
 public class GitTaskOne {
+	public static void password() {
+	     System.out.println("Password");
 
-}
+	   }
+		public static void main(String[] args) {
+			password();
+			
+		}
+	}
+
